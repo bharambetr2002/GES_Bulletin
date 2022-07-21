39d5668544bc16217d6174e56f1823cf39ad617a
Tanmay <bharambetr2002@gmail.com>
2022-07-21 22:36:03 +0530

    Project

---
+++ app/src/main/java/com/example/gesbulletin/DashBoardTeacherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gesbulletin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashBoardTeacherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board_teacher)
+    }
+}
