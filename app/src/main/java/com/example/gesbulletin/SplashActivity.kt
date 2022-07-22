ba6d0722dd914162f9854f7e5d1d6045d4ace99b
Tanmay <bharambetr2002@gmail.com>
2022-07-23 02:39:34 +0530

    Project

--- app/src/main/java/com/example/gesbulletin/SplashActivity.kt
+++ app/src/main/java/com/example/gesbulletin/SplashActivity.kt
@@ -4,15 +4,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         Handler().postDelayed(Runnable {
+            checkUser()
+        }, 2000) //2000 means 2 seconds
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
             startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        },2000) //means 2 seconds
+        }
+        else{
+            val ref = FirebaseDatabase.getInstance().getReference("Users")
+            ref.child(firebaseUser.uid)
+                .addListenerForSingleValueEvent(object : ValueEventListener
+                {
+                    override fun onDataChange(snapshot: DataSnapshot)
+                    {
+                        val userType = snapshot.child("userType").value
+                        if (userType == "user"){
+                            startActivity(Intent(this@SplashActivity, DashboardstudentActivity::class.java))
+                            finish()
+                        }
+                        else if (userType == "admin"){
+                            startActivity(Intent(this@SplashActivity, DashboardteacherActivity::class.java))
+                            finish()
+                        }
+                    }
+                    override fun onCancelled(error: DatabaseError)
+                    {
+
+                    }
+                })
+        }
     }
 }
